20a53860fa75f71702628a2b25c5dbf14215e6ec
paurbx <57190943+paurbx@users.noreply.github.com>
2020-07-28 16:08:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,13 @@ def answer():
     for l in range(3):
         if l == 0:
             print('1 загадка - я не вода, но по воде плаваю ')
-
             answer_for_one = 'льдина'
             input_for_one = input('Ваш ответ: ').lower()
 
             if input_for_one == answer_for_one:
                 print(f'Молодец! Ответ {input_for_one} правильный')
-                break
             else: 
-                print(f'Не правильный ответ, правильный ответ - {answer_for_one}')
-                break
+                print(f'Не правильный ответ, правильный ответ - {answer_for_one}
         elif l == 1:
             print('2 загадка - меня все топчут, а я все лучше) ')
 
@@ -22,10 +19,8 @@ def answer():
 
             if input_for_two == answer_for_two:
                 print(f'Молодец! Ответ {input_for_two} правильный')
-                break
             else: 
                 print(f'Не правильный ответ, правильный ответ - {answer_for_two}')
-                break
         
         elif l == 2:
             print('3 загадка - что невоможно удержать и 10 минут, хоть оно легче перышка? ')
@@ -35,9 +30,7 @@ def answer():
 
             if input_for_three == answer_for_three:
                 print(f'Молодец! Ответ {input_for_whree} правильный')
-                break
             else: 
                 print(f'Не правильный ответ, правильный ответ - {answer_for_three}')
-                break
 
 answer()
