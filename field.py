7859148309418c9962e66023c58f16a885775009
paurbx <57190943+paurbx@users.noreply.github.com>
2020-09-03 17:02:54 +0300

    Add files via upload

--- field.py
+++ field.py
@@ -14,55 +14,102 @@ def create_template(u):
     user_number = u
     x_pol = []
     for u in range(user_number):
-            x_pol.append('_' + ' ')
+            # x_pol.append('_' + ' ') - Зачем тут складывать две строки?
+            x_pol.append(['_'] * user_number) # Создание горизонтальных полей с помощью умножения.
+
+    #Выведем предварительный результат в консоль и убедимся, что код работает на этом моменте.
+    print('\nПоле после генерации')
+    [print(f) for f in x_pol]
     return x_pol
 
-
-def list_to_string_convert(t):
-    """
-    input: t - template (list)
-    output: s - list converted to string
-    """
-    s = ''
-    return s.join(t)
-
-
-def user_input_act():
-    '''
-    output: act of user(str)
-    '''
-    user_input_act = input('Для того чтобы перемещаться по полю использовать: up down right left и exit для выхода: ')
-    return user_input_act
-
-
-def the_character(act, x):
-    '''
-    input: act - act of user(str), x - x_pol(list)
-    output: character coordinates(list)[x, y]
-    '''
-    coord = [0, 0]
-    template = []
-    x.pop([coord[1]][coord[0]])
-    x.insert([coord[1]][coord[0]], 'x ')
-    for i in range
-    template.append(x)
+def player_move(template, direction):
+    # Проверяем если напарвление движения -> направо
+    if direction == 'right':
+        # Проходим по нашему полю с помощью цикла, для поиска координат нашего персонажа
+        for row in range(len(template)):
+            for col in range(len(template)):
+                # Если текущая ячейка содержит строку с персонажем
+                if template[row][col] == 'x':
+                    # Так как мы хотим двигаться направо
+                    # x _ _       _ x _
+                    # _ _ _   ->  _ _ _
+                    # _ _ _       _ _ _
+                    # Произошло банальное изменение индекса в колонке
+                    template[row][col] = '_'
+                    template[row][col+1] = 'x'
+                    # Запусти этот код в анализаторе и посмотри, что будет без ключевого слова break
+                    break
+    if direction == 'left':
+        # Проходим по нашему полю с помощью цикла, для поиска координат нашего персонажа
+        for row in range(len(template)):
+            for col in range(len(template)):
+                # Если текущая ячейка содержит строку с персонажем
+                if template[row][col] == 'x':
+                    # Так как мы хотим двигаться направо
+                    # x _ _       _ x _
+                    # _ _ _   ->  _ _ _
+                    # _ _ _       _ _ _
+                    # Произошло банальное изменение индекса в колонке
+                    template[row][col] = '_'
+                    template[row][col-1] = 'x'
+                    # Запусти этот код в анализаторе и посмотри, что будет без ключевого слова break
+                    break
+    if direction == 'up':
+        # Проходим по нашему полю с помощью цикла, для поиска координат нашего персонажа
+        for row in range(len(template)):
+            for col in range(len(template)):
+                # Если текущая ячейка содержит строку с персонажем
+                if template[row][col] == 'x':
+                    # Так как мы хотим двигаться направо
+                    # x _ _       _ x _
+                    # _ _ _   ->  _ _ _
+                    # _ _ _       _ _ _
+                    # Произошло банальное изменение индекса в колонке
+                    template[row][col] = '_'
+                    template[row+1][col] = 'x'
+                    # Запусти этот код в анализаторе и посмотри, что будет без ключевого слова break
+                    break
+    if direction == 'down':
+        # Проходим по нашему полю с помощью цикла, для поиска координат нашего персонажа
+        for row in range(len(template)):
+            for col in range(len(template)):
+                # Если текущая ячейка содержит строку с персонажем
+                if template[row][col] == 'x':
+                    # Так как мы хотим двигаться направо
+                    # x _ _       _ x _
+                    # _ _ _   ->  _ _ _
+                    # _ _ _       _ _ _
+                    # Произошло банальное изменение индекса в колонке
+                    template[row][col] = '_'
+                    template[row-1][col] = 'x'
+                    # Запусти этот код в анализаторе и посмотри, что будет без ключевого слова break
+                    break
     return template
 
 def game():
     status = True
     field_size = user_input_field()
-    while status:
-        template = create_template(field_size)
-        the_character(user_input_act, template)
-        to_print = list_to_string_convert(the_character)
 
-        for _ in range(field_size):
-            print(to_print)
+    # Проверяем работоспособность текущих функций  
+    template = create_template(field_size)
+    
+    # Если все работает как нам нужно, создаем базовое перемещение персонажа (например: направо)
+    # Для этого укажем базовую позицию игрока (например: слева-сверху)
+    template[0][0] = 'x'
 
-        user_act = user_input_act()
-
-        if user_act == 'exit':
-            status = False
+    # Обязательно проверяем на этот моменте, работоспособность
+    print('\nПоле после добавление игрока на базовую позицию 0, 0')
+    [print(f) for f in template]
         
+    # Создадим базовую функцию приема пользовательского ввода
+    #TODO Если все будет работать переделать на - usr_input = input('Укажите направление')
+    usr_input = input('Укажите направление из right left up down')
+
+    # Создадим базовый функционал перемещения с помощью функции player_move(template, direction)
+    template = player_move(template, usr_input)
+
+    # Обязательно проверяем на этот моменте, работоспособность
+    print('\nПоле после перемещения игрока на один индекс вправо 0, 1')
+    [print(f) for f in template]
 
 game()
